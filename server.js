bf2845838e85a56b0a0c758fdcda8f7f0716cf64
Anson Tang <ansontang@Ansons-MacBook-Pro.local>
2018-11-09 19:20:35 -0500

    added all files

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/js'));
+app.use(express.static(__dirname + '/fonts'));
+app.use(express.static(__dirname + '/datasets'));
+app.use(express.static(__dirname + '/stylesheets'));
+
+
+app.get('/',function(req,res){
+       
+     res.sendFile(__dirname +'/index.html');
+
+});
+
+app.listen(8080,function(){
+	console.log('ready at 8080');
+})
